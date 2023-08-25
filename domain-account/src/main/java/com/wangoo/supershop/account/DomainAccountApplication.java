c66969fe0e830e0bfc6953d47ee5e243794c372d
wangyanxin <wangyyyx@gmail.com>
2023-08-25 17:47:12 +0800

    初始版提交

---
+++ domain-account/src/main/java/com/wangoo/supershop/account/DomainAccountApplication.java
@@ -0,0 +1,14 @@
+package com.wangoo.supershop.account;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication(scanBasePackages = {"com.wangoo.supershop"})
+public class DomainAccountApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DomainAccountApplication.class, args);
+    }
+
+}
---
+++ domain-account/src/test/java/com/wangoo/supershop/account/DomainAccountApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wangoo.supershop.account;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DomainAccountApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
