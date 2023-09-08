ff4822e28d90b51be03ec58c74ce7bef879f93de
wangyanxin <wangyyyx@gmail.com>
2023-09-08 17:25:04 +0800

    新增gateway模块

---
+++ platform-gateway/src/test/java/com/wangoo/supershop/gateway/PlatformGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wangoo.supershop.gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlatformGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
